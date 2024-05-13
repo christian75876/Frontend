c98980a300cb310f976b996ed995499bfb276c99
Christian Pabon <127352132+christian75876@users.noreply.github.com>
2024-05-13 16:14:53 -0500

    primer commit

---
+++ Riwi/ritchie-olp-2024-1-js-fr/app/index.js
@@ -0,0 +1,6 @@
+import './styles/global.css';
+import { App } from './App';
+
+document.addEventListener('DOMContentLoaded', App);
+
+// Cuando cargue el archivo index.html, ejecuto lo que tiene por dentro App
---
+++ Riwi/ritchie-olp-2024-1-js-fr/webpack.config.js
@@ -0,0 +1,54 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'bundle'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext][query]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+    historyApiFallback: {
+      index: '/index.html', // Asegúrate de que devuelva correctamente index.html
+    }
+  }
+};
