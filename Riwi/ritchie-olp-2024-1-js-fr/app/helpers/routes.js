f5ca66e944bfada959858d8c17bd0a5308cfa3f4
Christian Pabon <christian75876@gmail.com>
2024-05-13 17:56:19 -0500

    Add last commit

--- Riwi/ritchie-olp-2024-1-js-fr/app/helpers/routes.js
+++ Riwi/ritchie-olp-2024-1-js-fr/app/helpers/routes.js
@@ -1,11 +1,12 @@
 import { LoginPage } from "../scenes/public/login";
-import { HomeScene, ReportScene } from "../scenes/private";
+import { HomeScene, ReportScene, AuditScene } from "../scenes/private";
+
 
 export const routes = {
   private: [
     { path: "/dashboard", component: HomeScene },
     { path: "/dashboard/reports", component: ReportScene },
-    { path: "/dashboard/audit", component: auditScene},
+    { path: "/dashboard/audit", component: AuditScene},
   ],
   public: [{ path: "/login", component: LoginPage }],
 };
