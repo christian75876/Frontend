1fba308770214da6d550f731902e199058d482d5
Christian Pabon <christian75876@gmail.com>
2024-05-13 17:44:57 -0500

    Add Page Audit in scenes

---
+++ Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
@@ -0,0 +1,11 @@
+import "./audit.css";
+import "../../../components/navigation-bar/navigation-bar";
+
+export function auditScene(){
+    const root = document.getElementById("root");
+
+    root.innerHTML = `
+    <navigation-bar id="navigation-bar"></navigation-bar>
+    <h2>Esta es pagina de auditoria</h2>
+    `
+}
