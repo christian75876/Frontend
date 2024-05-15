2955a33f5f8db00dff4f8b213d4dbcfd38991366
18zuluaga <18zuluaga1989@gmail.com>
2024-05-15 16:27:46 -0500

    Side bar in progress

--- Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/home/home.js
+++ Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/home/home.js
@@ -1,5 +1,6 @@
 import "./home.css";
 import "../../../components/sidebar-menu/sidebar-menu"
+import "../../../components/navigation-bar"
 
 export function HomeScene() {
   const root = document.getElementById("root");
