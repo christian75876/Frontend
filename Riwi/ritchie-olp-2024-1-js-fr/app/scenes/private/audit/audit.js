3ebe1134379484fde6144bc9d8a672798396ac9d
Christian Pabon <christian75876@gmail.com>
2024-05-15 12:05:20 -0500

    Add form filter

--- Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
+++ Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
@@ -6,8 +6,8 @@ export function AuditScene() {
 
   root.innerHTML = `
     <navigation-bar id="navigation-bar"></navigation-bar> 
-    <h2>Esta es pagina de auditoria</h2>
-    <button id="home">Prueba</button>
+    <h2>Audit Trail</h2>
+    <button id="home">New User</button>
     <div id="table">
         <table>
             <thead>
@@ -33,6 +33,8 @@ export function AuditScene() {
               </tr>
             </tbody>
         </table>
+        <section id="filter" style="display: none" >
+        </section>
         <section id="messageSection" style="display: none;">
       <p id="message"></p>
     </section>
@@ -46,12 +48,11 @@ export function AuditScene() {
   const messageParagraph = document.getElementById('message');
 
   btn.addEventListener('click', () => {
-    // Mostrar el mensaje
+    // Mostrar el formulario
     if (aux) return;
     messageParagraph.textContent = null;
     messageSection.style.display = 'block';
 
-    // Lógica adicional
     const messageHTML = `
       <form id="userForm">
         <label class="labelUser" for="firstName">Nombre:</label>
@@ -84,7 +85,7 @@ export function AuditScene() {
     // Agregar un evento para cerrar el mensaje
     const closeButton = document.getElementById('closeMessage');
     closeButton.addEventListener('click', () => {
-      // Ocultar el mensaje al hacer clic en el botón de cerrar
+      // Retornar a falso para que se pueda acceder nuevamente
       aux = false;
 
       messageSection.style.display = 'none';
@@ -92,4 +93,51 @@ export function AuditScene() {
   });
 
 
+  const menus = document.querySelectorAll('a');
+  const element = document.getElementById('filter');
+  let aux2 = false;
+  
+  menus.forEach(menu => {
+    menu.addEventListener('click', () => {
+      if (aux) return;
+      element.style.display = 'block';
+      const form = `
+      <form id="filterForm" class="filter-form">
+        <label class="filter-label" for="filterType">Filter By:</label>
+        <select class="filter-input" id="filterType" name="filterType">
+          <option value="userId">User ID</option>
+          <option value="date">Date</option>
+          <option value="clan">Clan</option>
+          <option value="role">Role</option>
+        </select>
+
+        <label class="filter-label" for="filterValue">Filter Value:</label>
+        <input class="filter-input" type="text" id="filterValue" name="filterValue">
+
+        <label class="filter-label" for="sortOrder">Sort Order:</label>
+        <select class="filter-input" id="sortOrder" name="sortOrder">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+
+        <button type="submit" class="filter-button">Filter</button>
+        <button id="closeFormButton" class="filter-button">Close</button>
+      </form>
+    `;
+      element.innerHTML += form;
+      aux2 = true;
+  
+      const closeButton = document.getElementById('closeFormButton');
+      closeButton.addEventListener('click', () => {
+        aux2 = false;
+        element.style.display = 'none';
+      });
+    });
+  });
+  
+
+
 }
+
+
+
