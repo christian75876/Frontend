a82626f7be83a3b2b16fceb3311bd1fb8081cea9
Christian Pabon <christian75876@gmail.com>
2024-05-15 10:05:28 -0500

    Add password in form new user

--- Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
+++ Riwi/ritchie-olp-2024-1-js-fr/app/scenes/private/audit/audit.js
@@ -39,50 +39,57 @@ export function AuditScene() {
     </div>
     `;
 
-    let aux = false;
+  let aux = false;
 
-    const btn = document.querySelector('button');
-    const messageSection = document.getElementById('messageSection');
-    const messageParagraph = document.getElementById('message');
+  const btn = document.querySelector('button');
+  const messageSection = document.getElementById('messageSection');
+  const messageParagraph = document.getElementById('message');
 
-    btn.addEventListener('click', () => {
-      // Mostrar el mensaje
-      if(aux) return;
-      messageParagraph.textContent = null;
-      messageSection.style.display = 'block';
-    
-      // Lógica adicional
-      const messageHTML = `
+  btn.addEventListener('click', () => {
+    // Mostrar el mensaje
+    if (aux) return;
+    messageParagraph.textContent = null;
+    messageSection.style.display = 'block';
+
+    // Lógica adicional
+    const messageHTML = `
       <form id="userForm">
-      <label class="labelUser" for="firstName">Nombre:</label>
-      <input class="inputUser" type="text" id="firstName" name="firstName" required>
-    
-      <label class="labelUser" for="lastName">Apellido:</label>
-      <input class="inputUser" type="text" id="lastName" name="lastName" required>
-    
-      <label class="labelUser" for="phone">Teléfono:</label>
-      <input class="inputUser" type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
-    
-      <label class="labelUser" for="clan">Clan:</label>
-      <input class="inputUser" type="text" id="clan" name="clan" required>
-    
-      <button type="submit">Crear Usuario</button>
-      <button id="closeMessage">Cerrar</button>
-    </form>       
+        <label class="labelUser" for="firstName">Nombre:</label>
+        <input class="inputUser" type="text" id="firstName" name="firstName" required>
+
+        <label class="labelUser" for="username">Usuario:</label>
+        <input class="inputUser" type="text" id="username" name="username" required>
+
+        <label class="labelUser" for="phone">Teléfono:</label>
+        <input class="inputUser" type="text" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+
+        <label class="labelUser" for="clan">Clan:</label>
+        <input class="inputUser" type="text" id="clan" name="clan" required>
+
+        <label class="labelUser" for="password">Contraseña:</label>
+        <input class="inputUser" type="password" id="password" name="password" required>
+
+        <label class="labelUser" for="confirmPassword">Confirmar Contraseña:</label>
+        <input class="inputUser" type="password" id="confirmPassword" name="confirmPassword" required>
+
+        <button type="submit">Crear Usuario</button>
+        <button id="closeMessage">Cerrar</button>
+      </form>
+      
       `;
 
-      messageParagraph.innerHTML += messageHTML;
-      aux = true;
-    
-      // Agregar un evento para cerrar el mensaje
-      const closeButton = document.getElementById('closeMessage');
-      closeButton.addEventListener('click', () => {
-        // Ocultar el mensaje al hacer clic en el botón de cerrar
-        aux = false;
+    messageParagraph.innerHTML += messageHTML;
+    aux = true;
 
-        messageSection.style.display = 'none';
-      });
+    // Agregar un evento para cerrar el mensaje
+    const closeButton = document.getElementById('closeMessage');
+    closeButton.addEventListener('click', () => {
+      // Ocultar el mensaje al hacer clic en el botón de cerrar
+      aux = false;
+
+      messageSection.style.display = 'none';
     });
-    
+  });
+
 
 }
